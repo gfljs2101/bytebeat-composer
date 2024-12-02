3b19801ab01d1b6fd7e87faae2674199240d43ad
GFLJS2100-user <reyedtreefrog@gmail.com>
2024-12-02 16:36:53 +0800

    Create GFLJS2100 - Cool music using funcbeat and charcodeat.js

---
+++ library/original/GFLJS2100 - Cool music using funcbeat and charcodeat.js
@@ -0,0 +1,4 @@
+return function (s,sr) {
+		let t=s*sr
+		t*=32768/sr
+		return ((((((asin(sin(s*"PiPiPiPiÆæÆæ88".charCodeAt(s*8&15)*15))))/4+(((sin(s*4*"PPPPPPPababab".charCodeAt(s*2&15)*15))))/4)+asin(sin(s*2048/256|(s*1)*128000/8)/2)/2))+(sin(40000/(t&(t&16384*4?t&16384*2?t&16384?t&8192?t&4096?254:511:1023:2047:4095:8191)))))/4+((t&16384?random()*255|t>>6:255)%256/128-2)/2+0.4}
