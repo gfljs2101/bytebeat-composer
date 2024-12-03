d5041aa715bb9b3de936234ca959b0bb09b122a8
gfljs2100-user3 <121681495+gfljs2100-user3@users.noreply.github.com>
2024-12-03 16:12:30 +0800

    Update bytebeat.mjs

--- scripts/bytebeat.mjs
+++ scripts/bytebeat.mjs
@@ -414,19 +414,19 @@ globalThis.bytebeat = new class {
 				codeBtn += `<button class="code-button code-load code-load-formatted" data-songdata='${
 					songData }' data-code-file="${ file
 				}" title="Click to load and play the formatted code">formatted ${
-				this.formatBytes(codeOriginal.length) }</button>`;
+				this.formatBytes(fileFormatted.length) }</button>`;
 			}
 			if(fileOriginal) {
 				codeBtn += `<button class="code-button code-load code-load-original" data-songdata='${
 					songData }' data-code-file="${ file
 				}" title="Click to load and play the original code">original ${
-				this.formatBytes(codeOriginal.length) }</button>`;
+				this.formatBytes(fileOriginal.length) }</button>`;
 			}
 			if(fileMinified) {
 				codeBtn += `<button class="code-button code-load code-load-minified" data-songdata='${
 					songData }' data-code-file="${ file
 				}" title="Click to load and play the minified code">minified ${
-				this.formatBytes(codeOriginal.length) }</button>`;
+				this.formatBytes(fileMinified.length) }</button>`;
 			}
 			if(codeBtn) {
 				entry += `<div class="code-buttons-container">${ codeBtn }</div>`;
