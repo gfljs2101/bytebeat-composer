5923e0ab380e65914bc333674f8e3f79165bf9e5
GFLJS2100-user <reyedtreefrog@gmail.com>
2024-12-10 17:20:47 +0800

    Update bytebeat.mjs

--- scripts/bytebeat.mjs
+++ scripts/bytebeat.mjs
@@ -424,17 +424,17 @@ globalThis.bytebeat = new class {
 			if(fileFormatted) {
 				codeBtn += `<button class="code-button code-load code-load-formatted" data-songdata='${
 					songData }' data-code-file="${ file
-				}" title="Click to load and play the formatted code">format ${this.formatBytes(file ? JSON.stringify(songObj) : ''.length) }</button>`;
+				}" title="Click to load and play the formatted code">format ${this.formatBytes(file.length) }</button>`;
 			}
 			if(fileOriginal) {
 				codeBtn += `<button class="code-button code-load code-load-original" data-songdata='${
 					songData }' data-code-file="${ file
-				}" title="Click to load and play the original code">orig ${this.formatBytes(file ? JSON.stringify(songObj) : ''.length) }</button>`;
+				}" title="Click to load and play the original code">orig ${this.formatBytes(file.length) }</button>`;
 			}
 			if(fileMinified) {
 				codeBtn += `<button class="code-button code-load code-load-minified" data-songdata='${
 					songData }' data-code-file="${ file
-				}" title="Click to load and play the minified code">min ${this.formatBytes(file ? JSON.stringify(songObj) : ''.length) }</button>`;
+				}" title="Click to load and play the minified code">min ${this.formatBytes(file.length) }</button>`;
 			}
 			if(codeBtn) {
 				entry += `<div class="code-buttons-container">${ codeBtn }</div>`;
